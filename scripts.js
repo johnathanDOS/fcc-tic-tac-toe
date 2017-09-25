ba2676bdcff29eee1f874fb5ec78505a519aca1d
johnathanDOS <jsonnen89@gmail.com>
2017-09-24 21:42:32 -0600

    Create function that allows computer to select cell

--- scripts.js
+++ scripts.js
@@ -1,47 +1,107 @@
-// Will need an object to track the game state.
-// This will keep track of whose turn it is.
 var cells = document.querySelectorAll('div.cell')
+var cellIds = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+console.log(cells)
 var gameState = {
 	player: 'X',
+	computer: 'O',
+	turn: 'player',
 	board: {
-		rowA: {
-			a1: '',
-			a2: '',
-			a3: '',
-		},
-		rowB: {
-			b1: '',
-			b2: '',
-			b3: '',
-		},
-		rowC: {
-			c1: '',
-			c2: '',
-			c3: '',
-		},
+		a1: '',
+		a2: '',
+		a3: '',
+		b1: '',
+		b2: '',
+		b3: '',
+		c1: '',
+		c2: '',
+		c3: '',
 	},
-
 }
 
-function switchPlayer () {
-	if (gameState.player === 'X') {
-		gameState.player = 'O'
+function nextTurn () {
+	if (gameState.turn === 'player') {
+		gameState.turn = 'computer'
 	} else {
-		gameState.player = 'X'
+		gameState.turn = 'player'
+	}
+}
+
+function win () {
+	//win condition 1: row a
+	if (gameState.board.a1 === gameState.board.a2 && 
+		gameState.board.a2 === gameState.board.a3 &&
+		gameState.board.a1 != '') {
+		return true
+	}
+	//win condition 2: row b
+	if (gameState.board.b1 === gameState.board.b2 && 
+		gameState.board.b2 === gameState.board.b3 &&
+		gameState.board.b1 != '') {
+		return true
+	}	
+	//win condition 3: row c
+	if (gameState.board.c1 === gameState.board.c2 && 
+		gameState.board.c2 === gameState.board.c3 &&
+		gameState.board.c1 != '') {
+		return true
+	}
+	//win condition 4: col 1
+	if (gameState.board.a1 === gameState.board.b1 && 
+		gameState.board.b1 === gameState.board.c1 &&
+		gameState.board.a1 != '') {
+		return true
+	}
+	//win condition 5: col 2
+	if (gameState.board.a2 === gameState.board.b2 && 
+		gameState.board.b2 === gameState.board.c2 &&
+		gameState.board.a2 != '') {
+		return true
+	}
+	//win condition 6: col 3
+	if (gameState.board.a3 === gameState.board.b3 && 
+		gameState.board.b3 === gameState.board.c3 &&
+		gameState.board.a3 != '') {
+		return true
+	}
+	//win condition 7: diagonal 1
+	if (gameState.board.a1 === gameState.board.b2 && 
+		gameState.board.b2 === gameState.board.c3 &&
+		gameState.board.a1 != '') {
+		return true
+	}
+	//win condition 8: diagonal 2
+	if (gameState.board.a3 === gameState.board.b2 && 
+		gameState.board.b2 === gameState.board.c1 &&
+		gameState.board.a3 != '') {
+		return true
 	}
 }
 
 function select (cell) {
-	document.getElementById(cell).innerHTML = gameState.player
-	switchPlayer()
-}
-
-console.log(cells);
-
-function buildGame() {
-	for (var i = 0; i < cells.length; i ++) {
-		cells[i].onclick = select();
+	if (gameState.board[cell.id] === '') {
+		cell.innerHTML = gameState[gameState.turn]
+		gameState.board[cell.id] = gameState[gameState.turn]
+		if (win()) {
+			console.log(gameState.turn + ' wins!')
+		} else {
+			nextTurn()
+			cpuRandSelect()
+		}
 	}
 }
 
-buildGame()
+function cpuRandSelect () {
+	var num = Math.floor(Math.random() * (8 - 0 + 1)) + 0
+	var selection = cellIds[num]
+	while (gameState.board[selection] != '') {
+		num = num = Math.floor(Math.random() * (8 - 0 + 1)) + 0
+		var selection = cellIds[num]
+	}
+	document.getElementById(selection).innerHTML = gameState.computer
+	gameState.board[selection] = gameState[gameState.turn]
+	if (win()) {
+		console.log(gameState.turn + ' wins!')
+	} else {
+		nextTurn()
+	}
+}
